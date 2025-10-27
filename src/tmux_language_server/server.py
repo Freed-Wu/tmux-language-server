dc033f5ea9aac1324171395c02b7b676fb93378d
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-10-27 15:02:03 +0800

    :arrow_up: Upgrade pygls

--- src/tmux_language_server/server.py
+++ src/tmux_language_server/server.py
@@ -25,7 +25,7 @@ from lsprotocol.types import (
     MarkupKind,
     TextDocumentPositionParams,
 )
-from pygls.server import LanguageServer
+from pygls.lsp.server import LanguageServer
 
 from .finders import DIAGNOSTICS_FINDER_CLASSES, ImportTmuxFinder
 from .utils import get_schema, parser
